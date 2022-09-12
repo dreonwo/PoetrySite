8fa4a1db34448c43105708f40edba9eefd58a999
dreonwo <dwheatleyowens@icloud.com>
2022-09-12 00:03:52 -0400

    Animations added

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,26 @@
+const navSlide = () =>{
+    const burger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+    const nav = document.querySelector('nav');
+    const links = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click',()=>{
+        navLinks.classList.toggle('nav-active');
+        burger.classList.toggle('hamburger-active');
+        nav.classList.toggle('navbar-active');
+
+        links.forEach((link, idx) => {
+            if(link.style.animation)
+                link.style.animation=''
+            else
+                link.style.animation = `navLinkFade .5s ease forwards ${idx/7 + .5}s`;
+        });
+
+        burger.classList.toggle('toggle');
+
+
+    });
+
+};
+
+navSlide();
